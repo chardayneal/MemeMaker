78140a1cb099cfec265ca18a1c6a7f00d276d74c
chardayneal <106034224+chardayneal@users.noreply.github.com>
2024-01-06 20:15:35 -0800

    Create UI  |  Configure segmented controls functionality, images, and labels

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var emojiSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //image array
+    let imageArray = ["issaInsecure", "mollyInsecure", "issaHappyInsecure", "kellyInsecure"]
+    
+    //caption array
+    let captionArray = ["Are you forreal?", "Imma do it anyway!", "Sounds good to me", "Stop playing with me", "Nevermind", "Let’s play the quiet game", "Don’t piss me off", "Are you sure about that?" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        changeImageView(imageSegmentedControl.selectedSegmentIndex)
+        changeCaption(emojiSegmentedControl.selectedSegmentIndex)
     }
 
+    @IBAction func segmentedControlPressed(_ sender: UISegmentedControl) {
+        //store index selection
+        
+        if sender == imageSegmentedControl {
+            changeImageView(sender.selectedSegmentIndex)
+        } else {
+            changeCaption(sender.selectedSegmentIndex)
+        }
+    }
+    
+    func changeImageView(_ index: Int) {
+        //depending on which segmented control option is selected, the respective image will be displayed in the imageView
 
+        imageView.image = UIImage(named: imageArray[index])
+    }
+    
+    func changeCaption(_ index: Int) {
+        //depending on which segmented control option is selected, the respective text will be displayed in the captionLabel
+        captionLabel.text = captionArray[index]
+   
+    }
+    
 }
 
